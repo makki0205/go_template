76ac964b40c234933dd30ae1eb05ffde58f846e5
makki0205 <llxo2_5oxll@icloud.com>
2017-12-07 10:31:14 +0900

    v3

--- model/db.go
+++ model/db.go
@@ -1,14 +1,17 @@
 package model
 
 import (
+	"io/ioutil"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"gopkg.in/yaml.v2"
 )
 
 var db = NewDBConn()
 
 func NewDBConn() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:password@tcp(127.0.0.1:3306)/{dbname}?parseTime=true")
+	db, err := gorm.Open(GetDBConfig("dbconfig.yml","development"))
 	if err != nil {
 		panic(err)
 	}
@@ -17,4 +20,19 @@ func NewDBConn() *gorm.DB {
 
 func GetDBConn() *gorm.DB {
 	return db
+}
+
+func GetDBConfig(configPath string, dbname string) (string, string) {
+	var buf, err = ioutil.ReadFile(configPath)
+	if err != nil {
+		panic(err)
+	}
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		panic(err)
+	}
+	driver := m[dbname].(map[interface{}]interface{})["dialect"].(string)
+	source := m[dbname].(map[interface{}]interface{})["datasource"].(string)
+	return driver, source
 }
